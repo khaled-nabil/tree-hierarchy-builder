070ce9544c577eb222108e45d87fa53f24a4eb19
khaled-nabil <khaled.nabil.k@gmail.com>
2019-03-02 15:37:51 +0300

    implementedd parseList & getChildren

--- application/lib/Tree/Tree.php
+++ application/lib/Tree/Tree.php
@@ -22,21 +22,48 @@ class Tree implements \IteratorAggregate, \JsonSerializable
     {
         $this->tree = self::parseList($list);
     }
-
+	//{ "Pete": "Nick", "Barbara": "Nick", "Nick": "Sophie", "Sophie": "Jonas" }
+	private function getChildren($name, $list, &$children) {
+		if(isset($children[$name])){
+			return $children[$name];
+		} else {
+			foreach($list as $child => $parent) {
+				if($name == $parent)
+					$children[$name][$child] = $this->getChildren($child,$list,$children);
+			}
+			if(isset($children[$name]))
+				return $children[$name];
+			else
+				return array();
+		}
+	}
     /**
      * Transform list-like assoc array [$child => $parent, $grandchild => $child]
      * into hierarchical (tree-like) assoc array [$parent => [$child => [$grandchild => []]
+	 * Assumptions: application/json content type already prevents duplicate definition of keys, thus loops are only possible at root.
+	 * In the case of a loop, no root will be found, thus throws logical exception
      *
      * @param array $list an assoc array with child as key, parent as value
      * @throws \InvalidArgumentException the input array was malformed
      * @throws RecursionException the input array was correctly formed but contains logic errors (e.g. loops)
      * @return array
      */
-    static public function parseList(array $list): array
+    public function parseList(array $list) : array
     {
-        // todo
-
-        return [];
+		$children = array();
+		$tree = array();
+		foreach($list as $child => $parent) {
+			if(!isset($list[$parent])) { // Start from roots down, if more than 1 root, print error.
+				if(sizeof($tree)> 0) {
+					throw new RecursionException("Tree structure can only have 1 root, roots found: ".key($tree)." and ".$parent);
+				}
+				$tree[$parent] = $this->getChildren($parent, $list, $children);
+			}
+		}
+		if(sizeof($tree) == 0 && sizeof($list)>0) { // no root found with valid data passed, caused by a loop
+			throw new RecursionException("You have a loop in your structure, structure must have a root node");
+		}
+		return $tree;
     }
 
     public function getArray(): array
